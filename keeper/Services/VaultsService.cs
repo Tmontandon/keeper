8942a89b31435a6d6408b8bd84ac25c2c5232374
Tmontandon <tt.montandon@gmail.com>
2022-11-05 03:34:37 -0600

    Vault beginning

---
+++ keeper/Services/VaultsService.cs
@@ -0,0 +1,14 @@
+namespace keeper.Services;
+
+public class VaultsService
+{
+  internal List<Vault> GetAllVaults()
+  {
+    throw new NotImplementedException();
+  }
+
+  internal Vault PostVault()
+  {
+    throw new NotImplementedException();
+  }
+}
