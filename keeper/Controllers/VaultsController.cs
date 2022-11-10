5d8292434bc94a3f445eac4a53100dbf9517359f
Tmontandon <tt.montandon@gmail.com>
2022-11-10 10:42:22 -0700

    Whoops this too

--- keeper/Controllers/VaultsController.cs
+++ keeper/Controllers/VaultsController.cs
@@ -30,11 +30,15 @@ public class VaultsController : ControllerBase
   }
 
   [HttpGet("{id}")]
-  public ActionResult<Vault> GetVaultById(int id)
+  public async Task<ActionResult<Vault>> GetVaultByIdAsync(int id)
   {
     try
     {
       Vault vault = _vs.GetVaultById(id);
+      if (vault.IsPrivate)
+      {
+        await CheckAuth(vault);
+      }
       return vault;
     }
     catch (Exception e)
@@ -43,12 +47,16 @@ public class VaultsController : ControllerBase
     }
   }
 
-
   [HttpGet("{id}/keeps")]
-  public ActionResult<List<VaultedKeep>> GetVksByVaultId(int id)
+  public async Task<ActionResult<List<VaultedKeep>>> GetVksByVaultIdAsync(int id)
   {
     try
     {
+      Vault v = _vs.GetVaultById(id);
+      if (v.IsPrivate)
+      {
+        await CheckAuth(v);
+      }
       List<VaultedKeep> keeps = _vks.GetKsByVId(id);
       return keeps;
     }
@@ -109,7 +117,16 @@ public class VaultsController : ControllerBase
     }
   }
 
-
+  [Authorize]
+  public async Task<bool> CheckAuth(Vault v)
+  {
+    Account userInfo = await _au.GetUserInfoAsync<Account>(HttpContext);
+    if (v.CreatorId != userInfo.Id)
+    {
+      throw new Exception("You are not authorized");
+    }
+    return true;
+  }
 
 
 }
